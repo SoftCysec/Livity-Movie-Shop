3350609fdada31f4c9c2bd6dfaeb5ef87e653ecb
softengdedan@gmail.com <softengdedan@gmail.com>
2023-01-01 15:53:33 +0300

    Popular Movies Added

--- myapp/views.py
+++ myapp/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render
 from .models import Movie
 
+import requests
+
 def movie_list(request):
-    movies = Movie.objects.all()
-    context = {'movies': movies}
-    return render(request, 'movies/movie_list.html', context)
+    api_key = 'ed224da74c443beeb88f1d204fe4e7a0'
+    api_url = f'https://api.themoviedb.org/3/movie/popular?api_key={api_key}'
+    response = requests.get(api_url)
+    movies = response.json()['results']
+    return render(request, 'movies/movie_list.html', {'movies': movies})
+
 
 def index(request):
     return render(request, 'movies/index.html', {})
