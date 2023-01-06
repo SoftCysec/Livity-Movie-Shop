8b8447ebcd30b80a936113e3f56058027ba42dc0
softengdedan@gmail.com <softengdedan@gmail.com>
2023-01-06 21:08:54 +0300

    allowed host modified

--- LivityDB/settings.py
+++ LivityDB/settings.py
@@ -29,7 +29,8 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ['livitydemba.up.railway.app','127.0.0.1','localhost']
+
 
 
 # Application definition
