ed1266308f49886f0af4560d894556b435228186
softengdedan@gmail.com <softengdedan@gmail.com>
2023-01-08 20:32:11 +0300

    System Update

--- accounts/views.py
+++ accounts/views.py
@@ -6,13 +6,17 @@ from accounts.decorators import login_required
 from accounts.forms import LogInForm, SignUpForm
 
 def signup(request):
+    if request.user.is_authenticated:
+        # Redirect the user to a suitable page if they are already authenticated
+        return redirect('myapp:index')
+
     if request.method == 'POST':
         form = SignUpForm(request.POST)
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
             raw_password = form.cleaned_data.get('password')
-            user = authenticate(username=username, password=raw_password)
+            user = authenticate(request, username=username, password=raw_password)
             login(request, user)
             return redirect('myapp:index')
     else:
@@ -21,13 +25,18 @@ def signup(request):
 
 
 
+
 def login_view(request):
+    if request.user.is_authenticated:
+        # user is already logged in, redirect to a suitable page
+        return redirect('myapp:index')
+
     if request.method == 'POST':
         form = LogInForm(request.POST)
         if form.is_valid():
-            username = form.cleaned_data.get('username')
-            password = form.cleaned_data.get('password')
-            user = authenticate(username=username, password=password)
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            user = authenticate(request, username=username, password=password)
             if user is not None:
                 login(request, user)
                 return redirect('myapp:index')
@@ -36,6 +45,7 @@ def login_view(request):
     else:
         form = LogInForm()
     return render(request, 'login.html', {'form': form})
+
 
 def logout_view(request):
     logout(request)
