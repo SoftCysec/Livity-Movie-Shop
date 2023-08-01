86ef9e2f9fe203dae530015d883f3f9d7d5fb912
Okware Dedan <108905313+SoftCysec@users.noreply.github.com>
2023-08-01 12:50:22 +0300

    production url modified

--- LivityDB/settings.py
+++ LivityDB/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['livityapi.up.railway.app','127.0.0.1','localhost']
+ALLOWED_HOSTS = ['livity-movie-shop-production.up.railway.app','127.0.0.1','localhost']
 
 
 
